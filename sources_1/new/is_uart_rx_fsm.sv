73210de9a31949c713e4e7e0159ddb0ea533fab2
Funky_Monkk <storozhev9003@gmail.com>
2024-11-11 19:16:58 +0300

    uart_rx_fsm without debugg

--- sources_1/new/is_uart_rx_fsm.sv
+++ sources_1/new/is_uart_rx_fsm.sv
@@ -43,13 +43,57 @@ import is_pkg_uart_controller::*;
                 end
             end        
             RSTRB: begin
+                if(rx_ce_i)
+                    if(rxd_rg_i) begin
+                        rxct_r_o <= '1;
+                        state <= IDLE;
+                    end
+                    else state <= RDT;
+                else state <= RSTRB;
             end
             
-            RDT:
-            RPARB:
-            RSTB1:
-            RSTB2:
-            WEND:             
+            RDT: begin
+                if(rx_ce_i) begin
+                    rx_data_t_o <= {rxd_rg_i,rx_data_t_o[7:1]};
+                    rx_data_cnt <= rx_data_cnt + 1'b1;
+                    if(rx_data_cnt == 4'h7) state <= RPARB;
+                    else state <= RDT;
+                end
+                else state <= RDT;
+            end
+            RPARB: begin
+                if(rx_ce_i) begin
+                    rx_data_t_o[8] <= '0; // Space 
+                    state <= RSTB1;
+                end
+                else state <= RPARB; 
+            end
+            
+            RSTB1: begin
+                if(rx_ce_i) begin
+                    $left(rx_data_t_o) <= ~rxd_rg_i;
+                    state <= RSTB2;
+                end
+                else state <= RSTB1;
+            end
+            RSTB2: begin
+                if(rx_ce_i) 
+                    if(rxd_rg_i) begin
+                        state <= IDLE;
+                        rx_data_en_o <= '1;
+                        rxct_r_o <= '1;
+                    end
+                    else state <= WEND;
+                else state <= RSTB2;
+            end
+            WEND:begin 
+                if(rxd_rg_i) begin
+                    state <= IDLE;
+                    rx_data_en_o <= '1;
+                    rxct_r_o <= '1;  
+                end  
+                else state <= WEND:
+            end          
         endcase
     end
 
