d84408f806b639b399b954287d167257f73e7eac
Funky_Monkk <storozhev9003@gmail.com>
2024-11-12 19:20:58 +0300

    rework x state typedef

--- sources_1/new/is_pkg_uart_controller.sv
+++ sources_1/new/is_pkg_uart_controller.sv
@@ -24,12 +24,12 @@ localparam ROM_W  = 128;
 
         typedef enum logic [2:0] {
             IDLE  = 3'b000,
-            RSTRB = 3'b001,
-            RDT   = 3'b010,
-            RPARB = 3'b011,
-            RSTB1 = 3'b100,
-            RSTB2 = 3'b101,
-            WEND  = 3'b110,
+            WCE = 3'b001,
+            TSTRB   = 3'b010,
+            TDT = 3'b011,
+            TPARB = 3'b100,
+            TSTB1 = 3'b101,
+            TSTB2  = 3'b110,
             XXX   = 'x } state_t;
 
 endpackage 
